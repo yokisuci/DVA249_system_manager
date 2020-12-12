6de10518b4cbfebbfad33f250ead614a50dbd796
Antônio Eklöf <antonioeklof@fastmail.com>
2020-12-12 00:18:52 +0000

    add: 'dialog' based menu

--- system_manager.bash
+++ system_manager.bash
@@ -4,33 +4,41 @@
 # --- PRINT MAIN MENU---
 # ----------------------
 
-function print_menu() {
-    echo "***********************"
-    echo "     SYSTEM MANAGER    "
-    echo "-----------------------"
-    echo
-    echo "ni - Network Info"
-    echo
-    echo "ua - User Add"
-    echo "ul - User List"
-    echo "uv - User View"
-    echo "um - User Modify"
-    echo "ud - User Delete"
-    echo
-    echo "ga - Group Add"
-    echo "gl - Group List"
-    echo "gv - Group View"
-    echo "gm - Group Modify"
-    echo "gd - Group Delete"
-    echo
-    echo "fa - Folder Add"
-    echo "fl - Folder List"
-    echo "fv - Folder View"
-    echo "fm - Folder Modify"
-    echo "fd - Folder Delete"
-    echo
-    echo "ex - Exit"
-    echo "-----------------------"
+# Define the dialog exit status codes
+: ${DIALOG_OK=0}
+: ${DIALOG_CANCEL=1}
+: ${DIALOG_HELP=2}
+: ${DIALOG_EXTRA=3}
+: ${DIALOG_ITEM_HELP=4}
+: ${DIALOG_ESC=255}
+
+function main_menu() {
+
+    CHOICE=$(dialog --clear \
+        --title "SYSTEM MANAGER" \
+        --menu "Select an option" \
+        15 0 5 \
+        n "Network Information" \
+        g "Group Management" \
+        u "User Management" \
+        f "Folder Management" \
+        2>&1 >/dev/tty) 
+
+    clear
+    case $CHOICE in
+        n)
+            network_info
+            ;;
+        g)
+            group_menu
+            ;;
+        u)
+            user_menu
+            ;;
+        f)
+            folder_menu
+            ;;
+    esac
 }
 
 # ---------------------------
@@ -38,13 +46,134 @@ function print_menu() {
 # ---------------------------
 
 function network_info() {
-    sleep 0
+    dialog --clear \
+        --title "SYSTEM MANAGER" \
+        --msgbox "Select an option" \
+        15 0
+
+    return_value=$?
+
+    # Act on it
+    case $return_value in
+        $DIALOG_OK)
+            main_menu
+            ;;
+        $DIALOG_ESC)
+            network_info
+            ;;
+    esac
 }
 
 
+# -----------------------
+# --- GROUP FUNCTIONS ---
+# -----------------------
+
+function group_menu() {
+
+    CHOICE=$(dialog --clear \
+        --title "GROUP MENU" \
+        --menu "Select an option" \
+        15 0 5 \
+        a "Add Group" \
+        l "List Group" \
+        v "View Group" \
+        m "Modify Group" \
+        d "Delete Group" \
+        2>&1 >/dev/tty) 
+
+    return_code=$?
+    if [[ $return_code == $DIALOG_CANCEL ]]; then
+        main_menu
+    elif [[ $return_code == $DIALOG_ESC ]]; then
+        group_menu
+    fi
+
+        clear
+        case $CHOICE in
+            a)
+                group_add
+                ;;
+            l)
+                group_list
+                ;;
+            v)
+                group_view
+                ;;
+            m)
+                group_modify
+                ;;
+            d)
+                group_delete
+                ;;
+        esac
+
+}
+
+function group_add() {
+    sleep 0
+}
+
+function group_list() {
+    sleep 0
+}
+
+function group_view() {
+    sleep 0
+}
+
+function group_modify() {
+    sleep 0
+}
+
+function group_delete() {
+    sleep 0
+}
+ö
 # ----------------------
 # --- USER FUNCTIONS ---
 # ----------------------
+
+function user_menu() {
+
+    CHOICE=$(dialog --clear \
+        --title "USER MENU" \
+        --menu "Select an option" \
+        15 0 5 \
+        a "Add User" \
+        l "List User" \
+        v "View User" \
+        m "Modify User" \
+        d "Delete User" \
+        2>&1 >/dev/tty) 
+
+    return_code=$?
+    if [[ $return_code == $DIALOG_CANCEL ]]; then
+        main_menu
+    elif [[ $return_code == $DIALOG_ESC ]]; then
+        user_menu
+    fi
+
+        clear
+        case $CHOICE in
+            a)
+                user_add
+                ;;
+            l)
+                user_list
+                ;;
+            v)
+                user_view
+                ;;
+            m)
+                user_modify
+                ;;
+            d)
+                user_delete
+                ;;
+        esac
+
+}
 
 function user_add() {
     sleep 0
@@ -66,33 +195,49 @@ function user_delete() {
     sleep 0
 }
 
-# -----------------------
-# --- GROUP FUNCTIONS ---
-# -----------------------
-
-function group_add() {
-    sleep 0
-}
-
-function group_list() {
-    sleep 0
-}
-
-function group_view() {
-    sleep 0
-}
-
-function group_modify() {
-    sleep 0
-}
-
-function group_delete() {
-    sleep 0
-}
-
 # ------------------------
 # --- FOLDER FUNCTIONS ---
 # ------------------------
+
+function folder_menu() {
+
+    CHOICE=$(dialog --clear \
+        --title "FOLDER MENU" \
+        --menu "Select an option" \
+        15 0 5 \
+        a "Add Folder" \
+        l "List Folder" \
+        v "View Folder" \
+        m "Modify Folder" \
+        d "Delete Folder" \
+        2>&1 >/dev/tty) 
+
+    return_code=$?
+    if [[ $return_code == $DIALOG_CANCEL ]]; then
+        main_menu
+    elif [[ $return_code == $DIALOG_ESC ]]; then
+        folder_menu
+    fi
+
+        clear
+        case $CHOICE in
+            a)
+                folder_add
+                ;;
+            l)
+                folder_list
+                ;;
+            v)
+                folder_view
+                ;;
+            m)
+                folder_modify
+                ;;
+            d)
+                folder_delete
+                ;;
+        esac
+}
 
 function folder_add() {
     sleep 0
@@ -114,7 +259,4 @@ function folder_delete() {
     sleep 0
 }
 
-while [[ $INPUT != "ex" ]]; do
-    print_menu
-    read -p "Choice: " INPUT
-done
+main_menu
